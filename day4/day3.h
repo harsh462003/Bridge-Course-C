c202f02773aeed7fcb133a3d0436340a5e0abce0
Harshith Nagaraj <148910831+harsh462003@users.noreply.github.com>
2025-08-02 03:18:40 +0530

    Add files via upload

---
+++ day4/day3.h
@@ -0,0 +1,7 @@
+void swap_two_numbers(int*, int*);//swap 2 numbers using pass by reference question 2
+int find_max_min(int arr[], int s, int *m, int *n);//question 3 again confirm about the size that i have to pass anything extra will cause problem in finding the min ele
+int insert_data(int arr[],int s);//question 5 enter elements but remove duplicate but this will make sure user enters 10 unique numbers
+int linear_search(int arr[],int s, int );//question 6
+int binarysearch(int arr[],int s, int);//question 7
+int productofconsecutive(int arr[],int s);
+void sorting(int arr[],int s);
